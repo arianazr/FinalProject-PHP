4477b06c82e546c86cb43702006d99f4c4ee1b53
Arian <arian.aziri50@gmail.com>
2023-05-13 15:02:42 +0200

    sixth commit

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,5 @@
-function validation() {
+function validation(event) {
+  event.preventDefault();
   let email = document.getElementById("email").value;
   let emailRegEx =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -6,19 +7,37 @@ function validation() {
   let password = document.getElementById("password").value;
   let passwordRegEx = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
+  let emailInput = document.getElementById("email");
+  let passwordInput = document.getElementById("password");
+
+  let popUp = document.querySelector(".signIn__popup");
   if (!email.match(emailRegEx)) {
-    document.getElementById("email").style.borderBottom = "2px solid red";
-    document.getElementById("email").classList.toggle("shake-animation");
+    emailInput.style.borderBottom = "2px solid #B00020";
+    emailInput.classList.toggle("shake-animation");
+    emailInput.style.color = "#B00020";
     return false;
   } else {
-    document.getElementById("email").style.borderBottom = "2px solid #000";
+    emailInput.style.borderBottom = "2px solid #000";
+    emailInput.style.color = "#000";
   }
   if (!password.match(passwordRegEx)) {
-    document.getElementById("password").style.borderBottom = "2px solid red";
-    document.getElementById("password").classList.toggle("shake-animation");
+    passwordInput.style.borderBottom = "2px solid #B00020";
+    passwordInput.classList.toggle("shake-animation");
+    passwordInput.style.color = "#B00020";
     return false;
   } else {
-    document.getElementById("password").style.borderBottom = "2px solid #000";
+    passwordInput.style.borderBottom = "2px solid #000";
+    passwordInput.style.color = "#000";
   }
+  event.target.reset();
+
+  if (email.match(emailRegEx) && password.match(passwordRegEx)) {
+    popUp.classList.add("signIn__popup--animated");
+    popUp.style.opacity = "1";
+  }
+
+  popUp.addEventListener("animationend", function () {
+    popUp.classList.remove("signIn__popup--animated");
+  });
   return true;
 }
--- js/store.js
+++ js/store.js
@@ -9,3 +9,32 @@ sortByBtn.addEventListener("click", () => {
     sortBy.setAttribute("aria-hidden", "false");
   }
 });
+
+let storeLogo = document.querySelector(".store-logo");
+let headerTitle = document.querySelector(".header__title");
+let clothesFilters = document.querySelector(".wrapper__items");
+let clothesFiltersChildren = clothesFilters.children;
+
+clothesFiltersChildren[0].addEventListener("click", () => {
+  headerTitle.style.display = "block";
+  headerTitle.textContent = "TOPS";
+  storeLogo.style.display = "none";
+});
+
+clothesFiltersChildren[1].addEventListener("click", () => {
+  headerTitle.style.display = "block";
+  headerTitle.textContent = "BOTTOMS";
+  storeLogo.style.display = "none";
+});
+
+clothesFiltersChildren[2].addEventListener("click", () => {
+  headerTitle.style.display = "block";
+  headerTitle.textContent = "FOOTWEAR";
+  storeLogo.style.display = "none";
+});
+
+clothesFiltersChildren[3].addEventListener("click", () => {
+  headerTitle.style.display = "block";
+  headerTitle.textContent = "ACCESORIES";
+  storeLogo.style.display = "none";
+});
